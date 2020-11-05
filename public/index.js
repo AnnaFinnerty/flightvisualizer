641b4d81bd9fc36e2f171e883c2cf6dfbef5e0dd
AnnaFinnerty <algfinnerty@gmail.com>
2020-11-04 23:03:05 -0700

    update express

--- public/index.js
+++ public/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 
 const app = express()
-const http = require('http');
-
 const port = 3000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.use('/', express.static('public'))
+app.use(express.static('public'))
 
 module.exports = app;
